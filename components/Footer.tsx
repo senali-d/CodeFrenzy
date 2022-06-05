1f2f253f3258a7c91d5fc697b18198d42a2f22d8
senali <senalidilumika@gmail.com>
2022-06-05 16:59:02 +0530

    Add header and footer components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,35 @@
+import { AiOutlineTwitter, AiFillGithub} from 'react-icons/ai'
+
 const Footer = () => {
   return (
-    <>Footer</>
+    <footer className="mx-auto max-w-[1080px] p-4 sm:p-6 sm:px-4 bg-white dark:bg-gray-800">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <a href="/" className="flex items-center mb-4 sm:mb-0">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">codechallenge</span>
+        </a>
+        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+          <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="" className="hover:underline">codechallenge</a>. All Rights Reserved.
+        </span>
+        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+          <a href="https://twitter.com/senali_d" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <AiOutlineTwitter />
+          </a>
+          <a href="https://github.com/senali-d" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <AiFillGithub />
+          </a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
