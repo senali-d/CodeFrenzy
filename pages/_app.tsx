2b65bda6a94a9a456f695d95e3197de2edccf920
senali <senalidilumika@gmail.com>
2022-06-05 17:10:16 +0530

    Add pages for navigation links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
